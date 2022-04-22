fcbca700e6286ac472651ed9fd3134f36850a114
bjwilliams5 <98039212+bjwilliams5@users.noreply.github.com>
2022-04-21 21:09:11 -0500

    Update build.R

--- r/build.R
+++ r/build.R
@@ -14,15 +14,24 @@ my_data <- my_data %>%
             hisp = substr(V1, 15, 15), sex = substr(V1, 16, 16),  age = substr(V1, 17, 18),
             pop = substr(V1, 19, 26))
 
-my_data <- my_data %>% 
-  select(!c("V1", "cfips", "reg")) %>% 
-  mutate(agegroup = case_when(
-    
-  ))
-
+my_data <- as.data.frame(my_data)0
+my_data$age <- as.numeric(my_data$age)
 my_data$pop <- as.numeric(my_data$pop)
+my_data$race <- as.numeric(my_data$race)
+my_data$hisp <- as.numeric(my_data$hisp)
+my_data$sex <- as.numeric(my_data$sex)
+
+
+my_data <- my_data %>% 
+  select(!c("V1", "cfips", "reg"))
+
+
+df <- my_data %>%
+  pivot_longer(!c(year, state, fips), names_to = "Year", values_to = "Pop")
 
 df <- my_data %>%
   group_by(year, state, fips, race, sex, hisp) %>% 
   summarize(population = sum(pop))
+  
+  
   
